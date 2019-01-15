4da7ee379e00bcb7c480f57ec6aa2b054e8eb291
AndreyDuda <andreyduda88@gmail.com>
2019-01-15 17:13:12 +0200

    phone verify unit test

---
+++ tests/Unit/Entity/User/PhoneTest.php
@@ -0,0 +1,153 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: duda
+ * Date: 02.01.19
+ * Time: 15:12
+ */
+
+namespace Tests\Unit\Entity\User;
+
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+use App\Entity\User;
+
+class PhoneTest extends TestCase
+{
+    use RefreshDatabase;
+
+   public function testDefault(): void
+   {
+       $user = factory(User::class)->create([
+           'phone'              => null,
+           'phone_verified'     => false,
+           'phone_verify_token' => null
+       ]);
+
+       self::assertFalse($user->isPhoneVerified());
+   }
+
+   public function testRequestEmptyPhone(): void
+   {
+       /** @var User $user */
+       $user = factory(User::class)->create([
+           'phone'              => null,
+           'phone_verified'     => false,
+           'phone_verify_token' => null
+       ]);
+
+       $this->expectExceptionMessage('Phone number is empty.');
+       $user->requestPhoneVerification(Carbon::now());
+   }
+
+   public function testRequest(): void
+   {
+       /** @var User $user */
+       $user = factory(User::class)->create([
+           'phone'              => '79000000000',
+           'phone_verified'     => false,
+           'phone_verify_token' => null
+       ]);
+
+       $token = $user->requestPhoneVerification(Carbon::now());
+
+       self::assertFalse($user->isPhoneVerified());
+       self::assertNotEmpty($token);
+   }
+
+   public function testRequestWithOldPhone(): void
+   {
+       /** @var User $user */
+       $user = factory(User::class)->create([
+           'phone'              => '79000000000',
+           'phone_verified'     => false,
+           'phone_verify_token' => null
+       ]);
+
+       self::assertTrue($user->isPhoneVerified());
+
+       $user->requestPhoneVerification(Carbon::now());
+
+       self::assertFalse($user->isPhoneVerified());
+       self::assertNotEmpty($user->phone_verify_token);
+   }
+
+   public function testRequestAlreadySentTimeOut(): void
+   {
+       /** @var User $user */
+       $user = factory(User::class)->create([
+           'phone'              => '79000000000',
+           'phone_verified'     => false,
+           'phone_verify_token' => null
+       ]);
+
+       $user->requestPhoneVerification($now = Carbon::now());
+       $user->requestPhoneVerification($now->copy()->addSeconds(500));
+
+       self::assertFalse($user->isPhoneVerified());
+   }
+
+   public function testRequestAlreadySent(): void
+   {
+       /** @var User $user */
+       $user = factory(User::class)->create([
+           'phone'              => '79000000000',
+           'phone_verified'     => false,
+           'phone_verify_token' => null
+       ]);
+
+       $user->requestPhoneVerification($now = Carbon::now());
+
+       $this->expectExceptionMessage('Token is already requested.');
+       $user->requestPhoneVerification($now->copy()->addSeconds(15));
+   }
+
+   public function testVerify(): void
+   {
+       /** @var User $user */
+       $user = factory(User::class)->create([
+           'phone'                     => '79000000000',
+           'phone_verified'            => false,
+           'phone_verify_token'        => $token = 'token',
+           'phone_verify_token_expire' => $now = Carbon::now()
+       ]);
+
+       self::assertFalse($user->isPhoneVerified());
+
+       $user->verifyPhone($token, $now->copy()->subSeconds(15));
+
+       self::assertTrue($user->isPhoneVerified());
+   }
+
+   public function testVerifyIncorrectToken(): void
+   {
+       /** @var User $user */
+       $user = factory(User::class)->create([
+           'phone'                     => '79000000000',
+           'phone_verified'            => false,
+           'phone_verify_token'        => 'token',
+           'phone_verify_token_expire' => $now = Carbon::now()
+       ]);
+
+       $this->expectExceptionMessage('Incorrect verify token.');
+       $user->verifyPhone('other_token', $now->copy()->subSeconds(15));
+   }
+
+   public function testVerifyExpiredToken(): void
+   {
+        /** @var User $user */
+        $user = factory(User::class)->create([
+            'phone'                     => '79000000000',
+            'phone_verified'            => false,
+            'phone_verify_token'        => $token = 'token',
+            'phone_verify_token_expire' => $now = Carbon::now()
+        ]);
+
+
+        $this->expectExceptionMessage('Token is expired.');
+        $user->verifyPhone($token, $now->copy()->subSeconds(500));
+   }
+
+}
