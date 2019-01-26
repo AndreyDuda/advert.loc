3be4662653e8722ae7aea2c194392522c78a49e0
AndreyDuda <andreyduda88@gmail.com>
2019-01-27 01:30:00 +0200

    correct logict

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -19,16 +19,55 @@ class AuthServiceProvider extends ServiceProvider
     {
         $this->registerPolicies();
 
+        Gate::define('horizon', function (User $user) {
+            return $user->isAdmin() || $user->isModerator();
+        });
+
         Gate::define('admin-panel', function (User $user) {
+            return $user->isAdmin() || $user->isModerator();
+        });
+
+        Gate::define('manage-pages', function (User $user) {
             return $user->isAdmin();
         });
 
-        Gate::define('shoe-advert', function(User $user, Advert $advert) {
+        Gate::define('manage-users', function (User $user) {
+            return $user->isAdmin() || $user->isModerator();
+        });
+
+        Gate::define('manage-tickets', function (User $user) {
+            return $user->isAdmin() || $user->isModerator();
+        });
+        Gate::define('manage-regions', function (User $user) {
+            return $user->isAdmin();
+        });
+
+        Gate::define('manage-adverts', function (User $user) {
+            return $user->isAdmin() || $user->isModerator();
+        });
+
+        Gate::define('manage-adverts-categories', function (User $user) {
+            return $user->isAdmin() || $user->isModerator();
+        });
+
+        Gate::define('manage-banners', function (User $user) {
+            return $user->isAdmin() || $user->isModerator();
+        });
+
+        Gate::define('show-advert', function (User $user, Advert $advert) {
             return $user->isAdmin() || $user->isModerator() || $advert->user_id === $user->id;
         });
 
-        Gate::define('edit-own-advert', function (User $user, Advert $advert) {
-            return $advert->user_id == $user->id;
+        Gate::define('manage-own-advert', function (User $user, Advert $advert) {
+            return $advert->user_id === $user->id;
+        });
+
+        Gate::define('manage-own-banner', function (User $user, Banner $banner) {
+            return $banner->user_id === $user->id;
+        });
+
+        Gate::define('manage-own-ticket', function (User $user, Ticket $ticket) {
+            return $ticket->user_id === $user->id;
         });
     }
 }
